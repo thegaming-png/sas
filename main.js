df96ef1d689157ca75b4b9a73eca1299af0eb182
thegaming-png <70883614+thegaming-png@users.noreply.github.com>
2021-10-23 17:27:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '">';
+    })
+}
+
+
+console.log('ml5 version:', ml5.version);
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hGtzneD6T/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+}
